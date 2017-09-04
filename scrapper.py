4b8ac1ede9921b6faa14a2ce64b77f389cc4871f
Mnickii <caroline.nkirote@andela.com>
2017-09-04 12:30:54 +0300

    ft(Add-scrapper-file): Code Refactor
    Signed-off-by: Mnickii <caroline.nkirote@andela.com>

--- scrapper.py
+++ scrapper.py
@@ -8,22 +8,22 @@ from bs4 import BeautifulSoup
 class Scrapper(object):
     def __init__(self):
         self.url = "https://www.standardmedia.co.ke"
-def scrape_site(self):
-    res = requests.get(self.url)
-    html = BeautifulSoup(res.content, 'html.parser')
-    if html:
-        div = html.find("div", class_="col-xs-6 col-md-6 zero")
-        ul = div.find_all("ul", class_="sub-stories-2")
-        data = []
-        for item in ul:
-            img_url = item.find("img").get("src")
-            text = item.find("img").get("alt")
-            link = item.find("li").find("a").get("href")
+    def scrape_site(self):
+        res = requests.get(self.url)
+        html = BeautifulSoup(res.content, 'html.parser')
+        if html:
+            div = html.find("div", class_="col-xs-6 col-md-6 zero")
+            ul = div.find_all("ul", class_="sub-stories-2")
+            data = []
+            for item in ul:
+                img_url = item.find("img").get("src")
+                text = item.find("img").get("alt")
+                link = item.find("li").find("a").get("href")
             data.append({
                 'title': text,
                 'link': link,
                 'img': img_url
                 })
             return json.dumps(data, indent=2)
-            scrapper = Scrapper()
-            print(scrapper.scrape_site())
+scrapper = Scrapper()
+print(scrapper.scrape_site())
